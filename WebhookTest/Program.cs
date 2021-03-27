06c789c03e23c7f8da1e7177acfb387ffcbeddf5
毕殿卿 <bdq123@outlook.com>
2021-03-27 21:29:24 +0800

    *

--- WebhookTest/Program.cs
+++ WebhookTest/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace WebhookTest
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
