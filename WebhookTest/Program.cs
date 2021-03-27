7a2f54ddaca25d7bff5d4b075779fe4324d5a386
bidianqing <bdq123@outlook.com>
2021-03-27 18:25:47 +0800

    *

--- WebhookTest/Program.cs
+++ WebhookTest/Program.cs
@@ -17,6 +17,7 @@ namespace WebhookTest
 
 
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
