84f4038b153f83b1a91f51a60ce778b6a79e0bfd
bidianqing <bdq123@outlook.com>
2021-03-27 18:10:40 +0800

    *

--- WebhookTest/build.sh
+++ WebhookTest/build.sh
@@ -8,3 +8,9 @@ cd /root/projects/WebhookTest/WebhookTest/
 dotnet build
 
 dotnet publish -c Release -o /var/publish/WebhookTest
+
+cd /var/publish/WebhookTest
+
+docker build -t webhooktest:$0 .
+docker tag webhooktest:$0 bidianqing/webhooktest:$0
+dokcer images
