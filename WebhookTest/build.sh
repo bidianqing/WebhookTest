4ddb2fe1f566c2626681a5151a743b5c34152bb6
bidianqing <bdq123@outlook.com>
2021-03-27 19:03:05 +0800

    *

--- WebhookTest/build.sh
+++ WebhookTest/build.sh
@@ -10,6 +10,6 @@ dotnet publish -c Release -o /var/publish/WebhookTest
 
 cd /var/publish/WebhookTest
 
-sudo docker build -t webhooktest:"${tag}" .
+sudo docker build -t webhooktest:${tag} .
 # sudo docker tag webhooktest:$imagetag bidianqing/webhooktest:$imagetag
 sudo docker images
